678309187576f55e1a9aaf317dfd889a56e8b828
aceeliu <aceeliu@qq.com>
2022-06-15 14:49:56 -0400

    added 2-period

---
+++ Two_Period.py
@@ -0,0 +1,53 @@
+#let d0 denotes ∆0, d_H denotes ∆1(H), d_T denotes ∆1(T)
+
+def get_pricing_measure(u,d,r):
+    p = (1+r-d)/(u-d)
+    q = (u-1-r)/(u-d)
+    return (p,q)
+
+
+def get_positive_part(x):
+    if x >= 0 : return x
+    else : return 0
+
+def main():
+    print("input your u : ")
+    u = input()
+    print("input your d : ")
+    d = input()
+    print("input your r : ")
+    r = input()
+    print("input your s0 : ")
+    s0 = input()
+    print("input your x0 : ")
+    x0 = input()
+    p,q = get_pricing_measure(u,d,r)
+    print("input your ∆0 : ")
+    d0 = input()
+    print("input your ∆1(H) : ")
+    d_H = input()
+    print("input your ∆1(T) : ")
+    d_T = input()
+    
+
+    bank_H = (1+r) * (u * s0 * (d0 - d_H) + (1+r) * (x0 - s0*d0))
+    bank_T = (1+r) * (u * s0 * (d0 - d_T) + (1+r) * (x0 - s0*d0))
+    x_HH = p * u ** 2 * s0 * d_H + bank_H
+    x_HT = q * u * d * s0 * d_H  + bank_H
+    x_TH = p * u * d * s0 * d_T  + bank_T
+    x_TT = q * d ** 2 * s0 * d_T  + bank_T
+
+    y0 = (1+r) ** (-2) * (p * (get_positive_part(x_HH) + get_positive_part(x_HT))
+    + q * (get_positive_part(x_TH) + get_positive_part(x_TT)))
+    return y0
+
+main()
+    
+    
+   
+    
+    
+
+
+
+    
