e4c9232430892cd9db9742d80fd28c6fc85cdfcb
aceeliu <aceeliu@qq.com>
2022-06-17 16:35:31 -0400

    N_Period

--- N_Period.py
+++ N_Period.py
@@ -10,28 +10,29 @@ def get_positive_part(x):
     if x >= 0 : return x
     else : return 0
 
-p,q = get_pricing_measure(u,d,r)
-def get_y0_k(s0, x0, k, n):
+# p,q = get_pricing_measure(u,d,r)
+# def get_y0_k(s0, x0, k, n):
     
-def get_y0(s0, x0, n, d): #implement formula of calculating y0
-    bank_H = (1+r) * (u * s0 * (d0 - d_H) + (1+r) * (x0 - s0*d0))
-    bank_T = (1+r) * (d * s0 * (d0 - d_T) + (1+r) * (x0 - s0*d0))
-    x_HH = p * (u ** 2 * s0 * d_H + bank_H)
-    x_HT = q * (u * d * s0 * d_H  + bank_H)
-    x_TH = p * (u * d * s0 * d_T  + bank_T)
-    x_TT = q * (d ** 2 * s0 * d_T  + bank_T)
-    
-    y0 = (1+r) ** (-n) * 
-    (p * (get_positive_part(x_HH) + get_positive_part(x_HT))
-    + q * (get_positive_part(x_TH) + get_positive_part(x_TT)))
-    # print("y0 is",y0)
-    return y0
+#def get_y0(s0, x0, n, d): #implement formula of calculating y0
+#   bank_H = (1+r) * (u * s0 * (d0 - d_H) + (1+r) * (x0 - s0*d0))
+#   bank_T = (1+r) * (d * s0 * (d0 - d_T) + (1+r) * (x0 - s0*d0))
+#   x_HH = p * (u ** 2 * s0 * d_H + bank_H)
+#   x_HT = q * (u * d * s0 * d_H  + bank_H)
+#   x_TH = p * (u * d * s0 * d_T  + bank_T)
+#   x_TT = q * (d ** 2 * s0 * d_T  + bank_T)
+#   
+#   y0 = (1+r) ** (-n) * 
+#   (p * (get_positive_part(x_HH) + get_positive_part(x_HT))
+#   + q * (get_positive_part(x_TH) + get_positive_part(x_TT)))
+#   # print("y0 is",y0)
+#   return y0
 
-def combination(n, curr, comb):
+def combination(n, curr, comb, curr_perm):
     if (curr == n) :
-        
+        comb.append(curr_perm)
     else:
-        combination(n, curr + 1)
+        combination(n, (curr + 1), comb, curr_perm.append(1))
+        combination(n, (curr + 1), comb, curr_perm.append(-1))
     
 #def combination_2(u, d, r, s0, x0):
 #   return [
@@ -44,20 +45,23 @@ def combination(n, curr, comb):
 #       get_y0(u,d,r,s0,x0,-1,-1,1),
 #       get_y0(u,d,r,s0,x0,-1,-1,-1)
 #   ]
-def main(n): #calculate according to your input 
+def main(): #calculate according to your input 
     # f = open("2period.txt", "r") 
     #combination_1(2, 0.5, 0.25, 8, 8)
     #combination_1(1.3, 0.94, 0.06, 8, 2)
-    comb = np.zeros((2**n, n))
-    combination(n, curr, comb)
-    y = np.zeros((101,8))
-    ystd = np.zeros(101)
-    for i in range(0, 100) :
-        y[i] = combination_2(1.3, 0.94, 0.06, 8, (i-50.0)*0.2)
-        ystd[i] = np.std(y[i])
-    x = np.arange(-10, 10.1, 0.2)
-    plt.plot(x, ystd)
-    plt.show()
+    #comb = np.zeros((2**n, n))
+    comb = []
+    combination(4, 1, comb, [])
+    print(comb)
+#   y = np.zeros((101,8))
+#   ystd = np.zeros(101)
+#   for i in range(0, 100) :
+#       y[i] = combination_2(1.3, 0.94, 0.06, 8, (i-50.0)*0.2)
+#       ystd[i] = np.std(y[i])
+#   x = np.arange(-10, 10.1, 0.2)
+#   plt.plot(x, ystd)
+#   plt.show()
+    
 #   print("input your u : ")
 #   u = float(input())
 #   print("input your d : ")
