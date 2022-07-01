e2c44ba3ab35a49e8e86bdf4d305ddbd4125f2ee
aceeliu <aceeliu@qq.com>
2022-07-01 16:02:08 -0400

    asd

--- np2.py
+++ np2.py
@@ -28,26 +28,29 @@ def get_yk(u, d, r, Sk, Xk, k, n, comb, i, l, cnt):  #return y
     q = 0.5
     # print("1 k= ", k, "cnt.nn=",cnt.nn)
     Xk_H = (1 + r)*(Xk - Sk * comb[i][cnt.nn]) + (Sk + l) * comb[i][cnt.nn]
-    if (k > 0) :
-        cnt.nn += 1
+#   if (k > 1) :
+#       cnt.nn += 1
     Xk_T = (1 + r)*(Xk - Sk * comb[i][cnt.nn]) + (Sk - l) * comb[i][cnt.nn]
-    if k < n - 2:
-        # print("2 k= ", k, "cnt.nn=",cnt.nn)
+    # print("cnt.nn = ", cnt.nn, "k = ", k, "Xk_H = ", Xk_H, "Xk_T = ", Xk_T)
+    if k < n:
+        # print("k = ", k, "cnt.nn =",cnt.nn)
         cnt.nn += 1
         y1 = get_yk((Sk + l)/Sk, (Sk - l)/Sk, r, Sk + l, Xk_H, (k + 1), n, comb, i, l, cnt)
         cnt.nn += 1
         y2 = get_yk((Sk + l)/Sk, (Sk - l)/Sk, r, Sk - l, Xk_T, (k + 1), n, comb, i, l, cnt)
+        # print("k = ", k, "y1 = ", y1, "y2 = ", y2)
         ret = (p * get_positive_part(y1) + q * get_positive_part(y2))
         return ret  
     else:
         # print("3 k= ", k, "cnt.nn=",cnt.nn)
         ret = (p * get_positive_part(Xk_H) + q * get_positive_part(Xk_T))
+        # print("ret = ", ret)
         return ret
     
     
 def get_y0(r, s0, x0, k, n, comb, id, l): #implement formula of calculating y0
     cnt.nn = 0
-    y0 = (1+r) ** (-n) * get_yk((s0 + l)/s0, (s0 - l)/s0, r, s0, x0, 0, n, comb, id, l, cnt)
+    y0 = (1+r) ** (-n) * get_yk((s0 + l)/s0, (s0 - l)/s0, r, s0, x0, 1, n, comb, id, l, cnt)
     return y0
     
 def combination(n, curr, comb, curr_perm):
@@ -77,12 +80,13 @@ def main(): #calculate according to your input
     curr_perm = []
     num_of_delta = 2**n-1
     combination(num_of_delta, 0, comb, curr_perm)
+    print(len(comb))
     maxx = 0 
     best_comb = []
     for i in range (2**num_of_delta):
         cnt.nn = 0
         ans = get_y0(r, s0, x0, 0, n, comb, i, l)
-        print("y0=",ans," with these deltas:",comb[i])
+        print("y0 =",ans," with these deltas:",comb[i])
         if (ans > maxx):
             maxx = ans
             best_comb = comb[i]
