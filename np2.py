461261e4b5231283d97be5634e03eab63b1d4745
lufeiyyy <98136967+lufeiyyy@users.noreply.github.com>
2022-07-01 13:33:41 -0400

    Update np2.py

--- np2.py
+++ np2.py
@@ -12,14 +12,14 @@ def get_positive_part(x):
     if x >= 0 : return x
     else : return 0
 
-def get_yk(u, d, r, Sk, Xk, k, n, comb, i, cnt, l) :  #return y
+def get_yk(u, d, r,Sk, Xk, k, n, comb, i, cnt) : #return y
     p,q = get_pricing_measure(u,d,r)
-    Xk_H = (1 + r)*(Xk - Sk * comb[i][cnt]) + (Sk + l)
+    Xk_H = (1 + r)*(Xk - Sk * comb[i][cnt]) + u * Sk * comb[i][cnt]
     #print("k=",k, " n=",n, "cnt=", cnt)
     if (k == 0) :
-        Xk_T = (1 + r)*(Xk - Sk * comb[i][cnt]) + (Sk - l)
+        Xk_T = (1 + r)*(Xk - Sk * comb[i][cnt]) + d * Sk * comb[i][cnt]
     else :
-        Xk_T = (1 + r)*(Xk - Sk * comb[i][cnt + 1]) + (Sk - l)
+        Xk_T = (1 + r)*(Xk - Sk * comb[i][cnt + 1]) + d * Sk * comb[i][cnt + 1]
     if (k == n - 1) :
         ret = (p * get_positive_part(Xk_H) + q * get_positive_part(Xk_T))
         return ret
@@ -28,15 +28,15 @@ def get_yk(u, d, r, Sk, Xk, k, n, comb, i, cnt, l) :  #return y
             new_cnt = cnt + 1
         else: 
             new_cnt = cnt + 2
-        y1 = get_yk((Sk + l)/Sk, (Sk - l)/Sk, r, Sk + l, Xk_H, (k + 1), n, comb, i, new_cnt, l)
-        y2 = get_yk((Sk + l)/Sk, (Sk - l)/Sk, r, Sk - l, Xk_T, (k + 1), n, comb, i, new_cnt, l)
+        y1 = get_yk(u, d, r, Sk*u, Xk_H, (k + 1), n, comb, i, new_cnt)
+        y2 = get_yk(u, d, r, Sk*d, Xk_T, (k + 1), n, comb, i, new_cnt)
         ret = (p * get_positive_part(y1) + q * get_positive_part(y2))
         return ret
         
-    
-def get_y0(u, d, r, s0, x0, k, n, comb, id, l): #implement formula of calculating y0
-    y0 = (1+r) ** (-n) * get_yk((s0 + l)/s0, (s0 - l)/s0, r, s0, x0, 0, n, comb, id, 0, l)
+def get_y0(u, d, r, s0, x0, k, n, comb, id): #implement formula of calculating y0
+    y0 = (1+r) ** (-n) * get_yk(u, d, r, s0, x0, 0, n, comb, id, 0)
     return y0
+    
 def combination(n, curr, comb, curr_perm):
     if (curr == n) :
         tmp = copy.copy(curr_perm)
@@ -63,8 +63,6 @@ def main(): #calculate according to your input
     x0 = float(input())
     print("input your n : ")
     n = int(input())
-    print("input your lambda : ")
-    l = float(input())
     comb = []
     curr_perm = []
     num_of_delta = 2**n-1
@@ -74,7 +72,7 @@ def main(): #calculate according to your input
     maxx = 0 
     best_comb = []
     for i in range (2**num_of_delta):
-        ans = get_y0(u, d, r, s0, x0, 0, n, comb, i, l)
+        ans = get_y0(u, d, r, s0, x0, 0, n, comb, i)
         print("y0=",ans," with these deltas:",comb[i])
         if (ans > maxx):
             maxx = ans
