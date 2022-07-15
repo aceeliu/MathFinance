4599e6058fdde3293705d342c378ccf75431b338
lufeiyyy <98136967+lufeiyyy@users.noreply.github.com>
2022-07-15 15:31:45 -0400

    Update randomize.py

--- randomize.py
+++ randomize.py
@@ -1,7 +1,7 @@
+#use Monte-Carlo algorithm to estimate V0 for high period models for sellers
+#use Las-Vegas algorithm to find strategy for buyers 
+
 #let d0 denotes ∆0, d_H denotes ∆1(H), d_T denotes ∆1(T)
-import matplotlib.pyplot as plt
-import numpy as np
-import copy
 import random
 
 from types import SimpleNamespace
@@ -10,8 +10,10 @@ cnt = SimpleNamespace(nn=0)
 def get_pricing_measure(u,d,r):
     p = (1+r-d)/(u-d)
     q = (u-1-r)/(u-d)
-    return (p,q)
-
+    if p >= 1 or p <= 0 or q >= 1 or q <= 0:
+        quit("Arbitrage")
+    else:
+        return (p,q)
 def get_positive_part(x):
     if x >= 0 : return x
     else : return 0
@@ -37,7 +39,7 @@ def get_y0(u, d, r, s0, x0, k, n, comb): #implement formula of calculating y0
     return y0,comb
 
 
-def main(): #calculate according to your input 
+def input_fn():
     print("input your u : ")
     u = float(input())
     print("input your d : ")
@@ -50,11 +52,14 @@ def main(): #calculate according to your input
     x0 = float(input())
     print("input your n : ")
     n = int(input())
+    return u,d,r,s0,x0,n
 
+u,d,r,s0,x0,n = input_fn()
+
+def mc(u,d,r,s0,x0,n): #calculate according to your input 
     maxx = 0
     avg_cnt = 0
     avg = 0
-
     for t in range (2**10):
        y0,comb = get_y0(u, d, r, s0, x0, 0, n, [])
        avg_cnt += 1
@@ -62,7 +67,18 @@ def main(): #calculate according to your input
        if (y0 > maxx):
            maxx = y0
            best_comb = comb
+    result = avg/avg_cnt
     print("final best:", best_comb, maxx)
-    print("avg:", avg/avg_cnt)
-   
-main()
+    print("average result:", result)
+    return result
+
+result = mc(u,d,r,s0,x0,n)
+
+def lv(u,d,r,s0,x0,n):
+    while True:
+        y0,comb = get_y0(u, d, r, s0, x0, 0, n, [])
+        if y0 > result:
+            print("strategy:", comb)
+            return comb
+
+lv(u,d,r,s0,x0,n)
